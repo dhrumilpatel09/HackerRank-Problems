c0c21943c5f697e5eb40e4760973bf0b411078df
Dhrumil Patel <138426097+dhrumilpatel09@users.noreply.github.com>
2025-05-10 13:23:54 +0530

    Solved JavaStdin.java

---
+++ Java-Solutions/JavaStdin.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        
+        int a = scan.nextInt();  // Read first integer
+        int b = scan.nextInt();  // Read second integer
+        int c = scan.nextInt();  // Read third integer
+
+        System.out.println(a);  // Print first integer
+        System.out.println(b);  // Print second integer
+        System.out.println(c);  // Print third integer
+    }
+}
